9f8e562776afe3b2dadbce9bf11a43e25a417169
ljnewediuk <j.newedi@gmail.com>
2024-01-26 14:45:22 -0600

    Rename

--- R/07-Fitted_Effects.R
+++ R/07-Fitted_Effects.R
@@ -2,6 +2,10 @@
 library(tidyverse)
 library(ggdist)
 
+# 07 - Extract fitted values from models
+
+# 1 Load data ====
+
 # Load life-history data
 lh_pop_dat <- readRDS('output/lh_info_pop.rds') %>%
   mutate(across(Born:LRS, list(sc = function(x) as.vector(scale(x, center = T)))))
@@ -13,11 +17,18 @@ lh_pop_dat_trunc <- lh_pop_dat %>%
   filter(Born %in% 1980:2000)
 
 # Load aging data
-epi_dat <- readRDS('input/PB_clock_ages2.rds') %>%
+epi_dat <- readRDS('output/PB_clock_ages.rds') %>%
+  # Add necessary columns
   rename('Year' = yr) %>%
   mutate(Born = Year - round(Age),
          across(c(Year, AgeAccel, Age, Born), 
                 list(sc = function(x) as.vector(scale(x, center = T)))))
+
+# Make aging data for males and females
+epi_dat_M <- epi_dat %>%
+  filter(Sex == 'M')
+epi_dat_F <- epi_dat %>%
+  filter(Sex == 'F')
 
 # List of models to load
 mods <- list.files('models/', pattern = 'mod')
@@ -28,23 +39,23 @@ for(n in 1:length(mods)) {
   assign(str_extract(mods[n], '[^.]+'), mod)
 }
 
+# 2 Fitted effects for age acceleration ~ year of birth (M & F separate) ====
 
-# Fitted effects for age accel ~ year born mod
-
-# New data for model
-# Female
+# Make new data for predicting
+# Female model
 nd_accel_born_F <- expand_grid(BearID = NA,
-                               Sex = 'F',
-                               Born_sc = seq(from = min(epi_dat$Born_sc), 
-                                             to = max(epi_dat$Born_sc),
+                               Born_sc = seq(from = min(epi_dat_F$Born_sc), 
+                                             to = max(epi_dat_F$Born_sc),
                                              by = 0.05))
-# Male
-nd_accel_born_M <- nd_accel_born_F %>%
-  mutate(Sex = 'M')
+# Male model
+nd_accel_born_M <- expand_grid(BearID = NA,
+                               Born_sc = seq(from = min(epi_dat_M$Born_sc), 
+                                             to = max(epi_dat_M$Born_sc),
+                                             by = 0.05))
 
-# Fitted effects
+# Extract fitted values
 # Female
-f_accel_born_F <- fitted(accel_born_mod,
+f_accel_born_F <- fitted(accel_born_mod_F,
             newdata = nd_accel_born_F,
             probs = c(0.025, 0.975),
             summary = F) %>%
@@ -57,7 +68,7 @@ f_accel_born_F <- fitted(accel_born_mod,
          AgeAccel = value * sd(epi_dat$AgeAccel) + mean(epi_dat$AgeAccel)) %>%
   select(Born, AgeAccel)
 # Male
-f_accel_born_M <- fitted(accel_born_mod,
+f_accel_born_M <- fitted(accel_born_mod_M,
                          newdata = nd_accel_born_M,
                          probs = c(0.025, 0.975),
                          summary = F) %>%
@@ -70,46 +81,16 @@ f_accel_born_M <- fitted(accel_born_mod,
          AgeAccel = value * sd(epi_dat$AgeAccel) + mean(epi_dat$AgeAccel)) %>%
   select(Born, AgeAccel)
 
-# Fitted effects for age at first repro ~ year model
 
-# New data for model
-nd_repro_year <- expand_grid(Sex = c('M', 'F'),
-                             BearID = NA,
-                             Born_sc = seq(from = min(lh_pop_dat_trunc$Born_sc), 
-                                           to = max(lh_pop_dat_trunc$Born_sc),
-                                           by = 0.05))
+# 3 Fitted effects for LRS ~ age at first reproduction model by decade ====
 
-# Fitted effects
-f_repro_year <- fitted(repro_yr_mod,
-                       newdata = nd_repro_year,
-                       probs = c(0.025, 0.975),
-                       summary = F) %>%
-  data.frame() %>%
-  # Pivot
-  pivot_longer(everything()) %>%
-  bind_cols(expand_grid(draws = 1:20000, nd_repro_year)) %>%
-  # Rename and unscale
-  mutate(Born = Born_sc * sd(lh_pop_dat$Born) + mean(lh_pop_dat$Born),
-         FirstRepro = value * sd(lh_pop_dat$FirstRepro) + mean(lh_pop_dat$FirstRepro)) %>%
-  select(Born, FirstRepro)
-
-# Plot
-f_repro_year %>%
-  ggplot(aes(x = Born, y = FirstRepro)) +
-  stat_lineribbon(.width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = 'grey') +
-  geom_jitter(data = lh_pop_dat_trunc, aes(x = Born, y = FirstRepro))
-
-# Fitted effects for lifetime repro success ~ age at first repro
-
-# New data for model
+# New data for model (-0.45, 0.45, and 1.35 correspond to 1980, 1990, 2000)
 nd_lrs_fr <- expand_grid(Born_sc = c(-0.45, 0.45, 1.35),
                          BearID = NA,
                          Sex = NA,
                          FirstRepro_sc = seq(from = min(lh_pop_dat_trunc$FirstRepro_sc), 
                                              to = max(lh_pop_dat_trunc$FirstRepro_sc),
                                              by = 0.05))
-
 # Fitted effects
 f_lrs_fr <- fitted(lrs_fr_mod,
                    newdata = nd_lrs_fr,
@@ -123,23 +104,7 @@ f_lrs_fr <- fitted(lrs_fr_mod,
   mutate(Born = floor(Born_sc * sd(lh_pop_dat$Born) + mean(lh_pop_dat$Born)),
          FirstRepro = FirstRepro_sc * sd(lh_pop_dat$FirstRepro) + mean(lh_pop_dat$FirstRepro))
 
-# Plot
-ggplot(f_lrs_fr, aes(x = FirstRepro, y = value)) +
-  stat_lineribbon(data = f_lrs_fr[f_lrs_fr$Born == 2000,],
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#193A82') +
-  stat_lineribbon(data = f_lrs_fr[f_lrs_fr$Born == 1990,],
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#6B84C0') +
-  stat_lineribbon(data = f_lrs_fr[f_lrs_fr$Born == 1980,],
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#BDD4FF') +
-  scale_colour_gradient(low = '#BDD4FF', high = '#193A82',
-                        breaks = c(-0.45, 0.45, 1.34), 
-                        labels = c(1980, 1990, 2000)) +
-  geom_jitter(data = lh_pop_dat_trunc, aes(x = FirstRepro, y = LRS, colour = Born_sc))
-
-# Fitted effects for age acceleration ~ age at first reproduction
+# 4 Fitted effects for age acceleration ~ age at first reproduction ====
 
 # New data for model
 nd_accel_fr_F <- expand_grid(Sex = 'F',
@@ -163,7 +128,6 @@ f_accel_fr_F <- fitted(accel_fr_mod,
   # Rename and unscale
   mutate(AgeAccel = value * sd(lh_epi_dat$AgeAccel) + mean(lh_epi_dat$AgeAccel),
          FirstRepro = FirstRepro_sc * sd(lh_epi_dat$FirstRepro) + mean(lh_epi_dat$FirstRepro))
-
 # Male
 f_accel_fr_M <- fitted(accel_fr_mod,
                        newdata = nd_accel_fr_M,
@@ -177,17 +141,8 @@ f_accel_fr_M <- fitted(accel_fr_mod,
   mutate(AgeAccel = value * sd(lh_epi_dat$AgeAccel) + mean(lh_epi_dat$AgeAccel),
          FirstRepro = FirstRepro_sc * sd(lh_epi_dat$FirstRepro) + mean(lh_epi_dat$FirstRepro))
 
-# Plot
-ggplot(f_accel_fr_M, aes(x = FirstRepro, y = AgeAccel)) +
-  stat_lineribbon(data = f_accel_fr_M,
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#6B84C0') +
-  stat_lineribbon(data = f_accel_fr_F,
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = 'green') +
-  geom_jitter(data = lh_epi_dat, aes(x = FirstRepro, y = AgeAccel, colour = Sex))
+# 5 Save fitted effects ====
 
-# Save fitted effects
 saveRDS(f_accel_born_F, 'output/f_effects_accel_born_F.rds')
 saveRDS(f_accel_born_M, 'output/f_effects_accel_born_M.rds')
 saveRDS(f_accel_fr_M, 'output/f_effects_accel_fr_M.rds')
--- R/08-Plots.R
+++ R/08-Plots.R
@@ -2,12 +2,15 @@
 library(tidyverse)
 library(cowplot)
 library(ggdist)
+library(bayesplot)
+library(brms)
 
-# Load fitted effects
+# 1 Load data ====
+
 # List of fitted effects to load
 fes <- list.files('output/', pattern = 'f_effects')
 
-# Load each model and assign name
+# Load fitted effects and assign name
 for(n in 1:length(fes)) {
   fes_n <- readRDS(paste0('output/', fes[n]))
   assign(str_extract(fes[n], '[^.]+'), fes_n)
@@ -24,13 +27,15 @@ lh_pop_dat_trunc <- lh_pop_dat %>%
   filter(Born %in% 1980:2000)
 
 # Load aging data
-epi_dat <- readRDS('input/PB_clock_ages2.rds') %>%
+epi_dat <- readRDS('output/PB_clock_ages.rds') %>%
   rename('Year' = yr) %>%
   mutate(Born = Year - round(Age),
          across(c(Year, AgeAccel, Age, Born), 
                 list(sc = function(x) as.vector(scale(x, center = T)))))
 
-# Get mean of posterior for age accel of F & M in accel ~ born model
+# 2 Plot age acceleration on year of birth ====
+
+# Get mean of posterior for age accel of F & M in accel ~ born models
 accel_born_mean_F <- f_effects_accel_born_F %>%
   group_by(Born) %>%
   summarize(AgeAccel = mean(AgeAccel))
@@ -38,7 +43,7 @@ accel_born_mean_M <- f_effects_accel_born_M %>%
   group_by(Born) %>%
   summarize(AgeAccel = mean(AgeAccel))
 
-# Plot age acceleration ~ birth year model
+# Plot age acceleration ~ birth year models (female and male models separately)
 ggplot(data = f_effects_accel_born_M, aes(x = Born, y = AgeAccel)) +
   stat_lineribbon(.width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#677daf') +
@@ -62,9 +67,11 @@ ggplot(data = f_effects_accel_born_M, aes(x = Born, y = AgeAccel)) +
         panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
   xlab('Year of birth') + ylab('Age acceleration (years)')
 
-# Save
+# Save plot
 ggsave('accel_born_plot.tiff', plot = last_plot(), path = 'figures/main/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
+
+# 3 Plot lifetime reproductive success on year of first reproduction ====
 
 # Get mean of posterior for all AFR in LRS ~ first repro model in 1980, 1990, 2000
 lrs_fr_means <- f_effects_lrs_fr %>%
@@ -103,9 +110,11 @@ ggplot(f_effects_lrs_fr, aes(x = FirstRepro, y = value)) +
         panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
   xlab('Age at first reproduction') + ylab('Lifetime reproductive success')
 
-# Save
+# Save plot
 ggsave('lrs_fr_plot.tiff', plot = last_plot(), path = 'figures/main/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
+
+# 4 Plot age acceleration ~ age at first reproduction ====
 
 # Get mean of posterior for age accel of F & M in accel ~ born model
 accel_fr_mean_F <- f_effects_accel_fr_F %>%
@@ -140,7 +149,45 @@ ggplot(f_effects_accel_fr_M, aes(x = FirstRepro, y = AgeAccel)) +
         panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
   ylab('Age acceleration (years)') + xlab('Age at first reproduction')
 
-# Save
+# Save plot
 ggsave('accel_fr_plot.tiff', plot = last_plot(), path = 'figures/main/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
+
+# 5 Plot posterior predictive checks ====
+
+mods <- list.files('models/', pattern = 'mod')
+
+color_scheme_set('red')
+
+# Load each model, plot pp_check, and assign plot name
+for(n in 1:length(mods)) {
+  mod <- readRDS(paste0('models/', mods[n]))
+  
+  y <- mod$data[,1]
+  yrep <- posterior_predict(mod, ndraws = 100)
+  mod_name <- str_extract(mods[n], '[^.]+')
+  
+  pp_plot <- ppc_dens_overlay(y, yrep) + 
+    theme(axis.text.x = element_text(size = 20, family = 'sans'),
+          legend.text = element_text(size = 20, family = 'sans'),
+          legend.position = 'none',
+          axis.line = element_line(colour = 'black', linewidth = 0.75),
+          plot.tag = element_text(size = 20, family = 'sans'),
+          plot.tag.position = 'top',
+          plot.margin = unit(rep(0.5, times = 4), 'cm')) +
+    labs(tag = mod_name)
+  
+  assign(paste0(mod_name, '_plot'), pp_plot)
+  
+}
+
+#  Make a list of plots
+all_plots <- list(accel_born_mod_plot, accel_fr_mod_plot, lrs_fr_mod_plot)
+
+# Plots in panels
+plot_grid(plotlist = all_plots, nrow = 1)
+
+# Save plots
+ggsave('pp_checks.tiff', plot = last_plot(), 
+       device = 'tiff', path = 'figures/', dpi = 300, height = 12, width = 25, units = 'cm', bg = 'white')
 
