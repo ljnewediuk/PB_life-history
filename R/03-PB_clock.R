dceea4a99291990676372cc2e637290278f1b9de
ljnewediuk <j.newedi@gmail.com>
2025-01-14 16:07:05 -0600

    clean up clock script to use function

--- R/03-PB_clock.R
+++ R/03-PB_clock.R
@@ -14,104 +14,26 @@
 library(tidyverse)
 library(glmnet)
 
+# Load samples that failed QC
+failed_QC <- readRDS('output/failed_QC_samples.rds')
+
 # Load related bears
 sibs <- readRDS('input/full_sibs.rds')
 
 # Load list of probes to include in clock
 probes <- readRDS('output/clock_Cpgs.rds')
 
-# Load chip sample sheets
-sample_sheet <- readRDS('output/updated_sample_sheet_PB_array1.rds') %>%
-  rbind(readRDS('output/updated_sample_sheet_PB_array2.rds')) %>%
-  rbind(readRDS('output/updated_sample_sheet_PB_array3.rds')) %>%
-  select(Sample_Name, chip.ID.loc) %>%
-  dplyr::rename('SampleID' = Sample_Name) %>%
-  distinct()
+# Source function for cleaning the betas
+source('functions/CleanBetas.R')
 
-# Load sample specs (i.e., sample type)
-samp_specs <- readRDS('input/batch1_samples.rds') %>%
-  rbind(readRDS('input/batch2_samples.rds')) %>%
-  rbind(readRDS('input/batch3_samples.rds')) %>%
-  select(sampleId, Spec, age, sex) %>%
-  dplyr::rename('SampleID' = sampleId, 'Age' = age, 'Sex' = sex) %>%
-  mutate(Spec = ifelse(Spec == '_Bloo', 'Blood', Spec)) %>%
-  left_join(sample_sheet) %>% distinct()
+# Clean betas in batches 1-3 and separate into training and testing
+meth_dat <- cleanBetas(batches = 1:3, failed_s = failed_QC, 
+                       excl_oth = sibs, keep_p = probes, sep_train = T)
 
-# Check which bears have multiple samples (need to exclude these too)
-multi_samps <- sample_sheet %>%
-  mutate(BearID = substr(SampleID, 1, 6)) %>%
-  group_by(BearID) %>%
-  summarize(n_samps = n()) %>%
-  filter(n_samps > 1) %>%
-  pull(BearID)
+# Specify training and testing data
+meth_betas_train <- meth_dat$train
+meth_betas_test <- meth_dat$test
 
-# 2 Calculate age to 0.25 yr ====
-
-# Calculate the actual age of the bear based on date of sample - age, assigning
-# a birth date of January 1 of year, then get quarterly age by 3 mos.
-birth_dates <- samp_specs %>%
-  mutate(birth_year = as.numeric(substr(SampleID, 8, 11)) - Age) %>%
-  mutate(sample_date = as.Date(substr(SampleID, 8, 17)),
-         birth_date = as.Date(paste0(birth_year, '-01-01')),
-         AgeDays = as.numeric(difftime(sample_date, birth_date, units = 'days')),
-         CorrectedAge = AgeDays/365,
-         CorrectedAgeQuarterly = ifelse(CorrectedAge - Age <= 0.25, Age + 0.25, 0),
-         CorrectedAgeQuarterly = ifelse(CorrectedAge - Age > 0.25 & 
-                                          CorrectedAge - Age <= 0.5, Age + 0.5, 
-                                        CorrectedAgeQuarterly),
-         CorrectedAgeQuarterly = ifelse(CorrectedAge - Age > 0.5 & 
-                                          CorrectedAge - Age <= 0.75, Age + 0.75, 
-                                        CorrectedAgeQuarterly),
-         CorrectedAgeQuarterly = ifelse(CorrectedAge - Age > 0.75 & 
-                                          CorrectedAge - Age <= 0.99, Age + 1, 
-                                        CorrectedAgeQuarterly)) %>%
-  select(! c(Age, birth_year:CorrectedAge)) %>%
-  dplyr::rename('Age' = CorrectedAgeQuarterly)
-
-# 3 Load sample matrices ====
-
-# Combine matrices fro first three arrays and exclude outliers
-meth_betas <- readRDS('output/tbetas_PB_array1.rds') %>%
-  rbind(readRDS('output/tbetas_PB_array2.rds')) %>%
-  rbind(readRDS('output/tbetas_PB_array3.rds')) %>%
-  rownames_to_column('chip.ID.loc') %>%
-  left_join(birth_dates) %>%
-  # Create column with ID to exclude related bears
-  mutate(BearID = substr(SampleID, 1, 6)) %>%
-  # Move ID columns to front
-  relocate(c(SampleID, BearID, Age, Sex, Spec, chip.ID.loc)) %>%
-  # Exclude outlier samples (failed scans/wierd samples)
-  # ßs cluster away from others in array 1: 
-  #   - X10228_1998_08-31_Blood
-  #   - X10776_1997-09-06_Blood
-  # ßs cluster away from others in array 2:
-  #   - X03367_1997-09-22_Blood
-  # ßs cluster away from others in array 2:
-  #   - X12697_2008-09-16_Skin
-  #   - X12697_1997_09_22_Blood
-  #   - X09365_1988-09-29_Blood
-  #   - X03292_2001-09-15_Blood
-filter(! SampleID %in% c('X09304_2001-09-06_Blood', 'X09407_1988-09-07_Blood',
-                         'X12606_1997-08-28_Blood', 'X10776_1997-09-06_Blood', 
-                         'X10228_1998-08-31_Blood', 'X03292_2001-09-15_Blood',
-                         'X12697_2008-09-16_Skin', 'X09365_1988-09-29_Blood',
-                         'X12697_1997-09-22_Blood')) %>%
-  # Select only probes we want to include based on EWAS
-  select(c(SampleID:chip.ID.loc, all_of(probes)))
-
-# 4 Make testing and training data ====
-
-# Training betas
-meth_betas_train <- meth_betas %>%
-  # Exclude bears that are full sibs
-  filter(! BearID %in% sibs) %>%
-  # Exclude bears that have multiple samples
-  filter(! BearID %in% multi_samps)
-
-# Testing betas
-meth_betas_test <- meth_betas %>%
-  # Exclude bears in test set
-  filter(! BearID %in% meth_betas_train$BearID)
 
 # List IDs of training and testing bears
 train_bears <- meth_betas_train %>%
@@ -140,7 +62,7 @@ meth_betas_test_m <- meth_betas_test %>%
 # 5 Check ages ====
 
 # Add ages for training and testing
-age_df <- meth_betas %>%
+age_df <- meth_betas_train %>%
   mutate(AgePredict = 0) %>%
   select(SampleID:chip.ID.loc, AgePredict)
 
@@ -159,8 +81,8 @@ set.seed(2)
 cvfit <- cv.glmnet(betasLoop, ageLoop, nfolds = 10, alpha = .5)
 
 # Add predictions as column to ages in training data
-age_preds <- age_df %>%
-  filter(SampleID %in% meth_betas_test$SampleID) %>%
+age_preds <- meth_betas_test %>%
+  select(SampleID:Spec) %>%
   # Predict model
   mutate(AgePredict = as.numeric(predict(cvfit, newx = meth_betas_test_m, 
                                          type = "response", s = "lambda.min")))
@@ -168,9 +90,12 @@ age_preds <- age_df %>%
 age_preds <-  age_preds %>%
   mutate(AgeAccel = lm(age_preds$AgePredict ~ age_preds$Age)$residuals,
          # Add year
-         yr = as.numeric(substr(SampleID, 8, 11))) %>%
-  # Remove chip column
-  select(! chip.ID.loc)
+         yr = as.numeric(substr(SampleID, 8, 11)),
+         Born = yr - floor(Age)) %>%
+  group_by(BearID) %>%
+  summarize(AgeAccel = mean(AgeAccel), Born = unique(Born))
+
+summary(lm(AgeAccel ~ Born, data = age_preds))
 
 # 7 Plot clock ====
 
