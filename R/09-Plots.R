a0c71ae109e6887f3f0c56c39ee4b3ea95c3a082
ljnewediuk <j.newedi@gmail.com>
2025-01-16 16:30:03 -0600

    update plots with new data

--- R/09-Plots.R
+++ R/09-Plots.R
@@ -30,19 +30,29 @@ for(n in 1:length(fes)) {
 
 # Load life-history data
 lh_pop_dat <- readRDS('output/lh_info_pop.rds') %>%
-  mutate(across(Born:LRS, list(sc = function(x) as.vector(scale(x, center = T)))))
-lh_epi_dat <- readRDS('output/lh_info_epi.rds') %>%
-  mutate(across(c(Born:LRS, AgeAccel), list(sc = function(x) as.vector(scale(x, center = T)))))
+  filter(! is.infinite(FirstRepro) | ! is.infinite(LastRepro)) %>%
+  # Scale and centre variables
+  mutate(across(Born:LRS, list(sc = function(x) as.vector(scale(x, center = T))))) %>%
+  # Filter individuals born after 2000 (we might not have captured their full
+  # reproductive lifespan)
+  filter(Born <= 2000 & Born >= 1980)
 
-# Limit life history data to only reliable sampling dates
-lh_pop_dat_trunc <- lh_pop_dat %>%
-  filter(Born %in% 1980:2000)
+# With epigenetic data
+lh_epi_dat <- readRDS('output/lh_info_epi.rds') %>%
+  # Summarize by individual
+  group_by(BearID) %>%
+  summarize(AgeAccel = mean(AgeAccel), FirstRepro = mean(FirstRepro)) %>%
+  # Scale and centre variables
+  filter(! is.infinite(FirstRepro)) %>%
+  mutate(across(c(AgeAccel, FirstRepro), list(sc = function(x) as.vector(scale(x, center = T)))))
 
 # Load aging data
-epi_dat <- readRDS('output/PB_clock_ages.rds') %>%
-  rename('Year' = yr) %>%
-  mutate(Born = Year - round(Age),
-         across(c(Year, AgeAccel, Age, Born), 
+epi_dat <- readRDS('output/WH_combined_ages.rds') %>%
+  # Summarize by individual
+  group_by(BearID) %>%
+  summarize(AgeAccel = mean(AgeAccel), Born = mean(Born)) %>%
+  # Scale and centre variables
+  mutate(across(c(AgeAccel, Born),
                 list(sc = function(x) as.vector(scale(x, center = T)))))
 
 # 2 Plot age acceleration on year of birth ====
@@ -51,24 +61,13 @@ epi_dat <- readRDS('output/PB_clock_ages.rds') %>%
 accel_born_mean <- f_effects_accel_born %>%
 group_by(Born) %>%
   summarize(AgeAccel = mean(AgeAccel))
-accel_born_mean_F <- f_effects_accel_born_F %>%
-  group_by(Born) %>%
-  summarize(AgeAccel = mean(AgeAccel))
-accel_born_mean_M <- f_effects_accel_born_M %>%
-  group_by(Born) %>%
-  summarize(AgeAccel = mean(AgeAccel))
 
 # Plot age acceleration ~ birth year models (female and male models separately)
-ggplot(data = f_effects_accel_born_M, aes(x = Born, y = AgeAccel)) +
+ggplot(data = f_effects_accel_born, aes(x = Born, y = AgeAccel)) +
   stat_lineribbon(.width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#677daf') +
-  stat_lineribbon(data = f_effects_accel_born_F, 
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#86b7b9') +
-  geom_line(data = accel_born_mean_M, colour = '#425d9c') +
-  geom_line(data = accel_born_mean_F, colour = '#68A6A8') +
-  geom_point(data = epi_dat, aes(x = Born, y = AgeAccel, colour = Sex), size = 3) +
-  scale_color_manual(values = c('#68A6A8', '#425d9c')) +
+  geom_line(data = accel_born_mean, colour = '#425d9c') +
+  geom_point(data = epi_dat, aes(x = Born, y = AgeAccel), colour = '#425d9c', size = 3) +
   theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1.25),
         axis.text = element_text(size = 18, colour = 'black'),
         axis.title.y = element_text(size = 18, colour = 'black', vjust = 3),
@@ -95,21 +94,21 @@ lrs_fr_means <- f_effects_lrs_fr %>%
 
 # Plot LRS ~ first repro
 ggplot(f_effects_lrs_fr, aes(x = FirstRepro, y = value)) +
-  geom_jitter(data = lh_pop_dat_trunc, aes(x = FirstRepro, y = LRS, colour = Born_sc), size = 3) +
+  geom_jitter(data = lh_pop_dat, aes(x = FirstRepro, y = LRS, colour = Born_sc), size = 3) +
   stat_lineribbon(data = f_effects_lrs_fr[f_effects_lrs_fr$Born == 2000,],
                   .width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#193A82') +
-  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 2000,], colour = '#112757') +
+  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 2000,], colour = '#102A5C') +
   stat_lineribbon(data = f_effects_lrs_fr[f_effects_lrs_fr$Born == 1990,],
                   .width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#6B84C0') +
-  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 1990,], colour = '#4a67ae') +
+  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 1990,], colour = '#5268A0') +
   stat_lineribbon(data = f_effects_lrs_fr[f_effects_lrs_fr$Born == 1980,],
                   .width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#BDD4FF') +
-  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 1980,], colour = '#8ab3ff') +
+  geom_line(data = lrs_fr_means[lrs_fr_means$Born == 1980,], colour = '#94A9CC') +
   scale_colour_gradient(low = '#BDD4FF', high = '#193A82',
-                        breaks = c(-0.45, 0.45, 1.34), 
+                        breaks = c(-0.43, 0.45, 1.3), 
                         labels = c(1980, 1990, 2000),
                         name = 'Year born') +
   theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1.25),
@@ -131,26 +130,18 @@ ggsave('lrs_fr_plot.tiff', plot = last_plot(), path = 'figures/main/',
 
 # 4 Plot age acceleration ~ age at first reproduction ====
 
-# Get mean of posterior for age accel of F & M in accel ~ born model
-accel_fr_mean_F <- f_effects_accel_fr_F %>%
-  group_by(FirstRepro) %>%
-  summarize(AgeAccel = mean(AgeAccel))
-accel_fr_mean_M <- f_effects_accel_fr_M %>%
+# Get mean of posterior for age accel in accel ~ born model
+accel_fr_mean <- f_effects_accel_fr %>%
   group_by(FirstRepro) %>%
   summarize(AgeAccel = mean(AgeAccel))
 
 # Plot age accel ~ first repro
-ggplot(f_effects_accel_fr_M, aes(x = FirstRepro, y = AgeAccel)) +
-  stat_lineribbon(data = f_effects_accel_fr_M,
+ggplot(f_effects_accel_fr, aes(x = FirstRepro, y = AgeAccel)) +
+  stat_lineribbon(data = f_effects_accel_fr,
                   .width = seq(from = .03, to = .975, by = .03),
                   alpha = .1, size = 0, fill = '#6B84C0') +
-  geom_line(data = accel_fr_mean_M, colour = '#425d9c') +
-  stat_lineribbon(data = f_effects_accel_fr_F,
-                  .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = '#86b7b9') +
-  geom_line(data = accel_fr_mean_F, colour = '#68A6A8') +
-  scale_colour_manual(values = c('#68A6A8', '#425d9c')) +
-  geom_jitter(data = lh_epi_dat, aes(x = FirstRepro, y = AgeAccel, colour = Sex), size = 3) +
+  geom_line(data = accel_fr_mean, colour = '#425d9c') +
+  geom_jitter(data = lh_epi_dat, aes(x = FirstRepro, y = AgeAccel), colour = '#425d9c', size = 3) +
   theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1.25),
         axis.text = element_text(size = 18, colour = 'black'),
         axis.title.y = element_text(size = 18, colour = 'black', vjust = 3),
@@ -165,7 +156,7 @@ ggplot(f_effects_accel_fr_M, aes(x = FirstRepro, y = AgeAccel)) +
   ylab('Age acceleration (years)') + xlab('Age at first reproduction')
 
 # Save plot
-ggsave('accel_fr_plot.tiff', plot = last_plot(), path = 'figures/extended/', 
+ggsave('accel_fr_plot.tiff', plot = last_plot(), path = 'figures/main/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
 
 # 5 Plot posterior predictive checks ====
@@ -183,13 +174,16 @@ for(n in 1:length(mods)) {
   mod_name <- str_extract(mods[n], '[^.]+')
   
   pp_plot <- ppc_dens_overlay(y, yrep) + 
-    theme(axis.text.x = element_text(size = 20, family = 'sans'),
-          legend.text = element_text(size = 20, family = 'sans'),
+    theme(panel.background = element_rect(colour = 'black', 
+                                          fill = 'white', linewidth = 1.25),
+          axis.text.x = element_text(size = 20, family = 'sans', colour = 'black'),
+          legend.text = element_text(size = 20, family = 'sans', colour = 'black'),
           legend.position = 'none',
           axis.line = element_line(colour = 'black', linewidth = 0.75),
-          plot.tag = element_text(size = 20, family = 'sans'),
+          plot.tag = element_text(size = 20, family = 'sans', colour = 'black'),
           plot.tag.position = 'top',
-          plot.margin = unit(rep(0.5, times = 4), 'cm')) +
+          plot.margin = unit(rep(0.5, times = 4), 'cm'),
+          panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
     labs(tag = mod_name)
   
   assign(paste0(mod_name, '_plot'), pp_plot)
