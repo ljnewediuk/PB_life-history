2eebab68978f0498124ccf6cb621d8b20e6a50a9
ljnewediuk <j.newedi@gmail.com>
2025-01-22 18:27:13 -0600

    update with new data, add sex model

--- R/X6-Check_tissues.R
+++ R/X6-Check_tissues.R
@@ -13,92 +13,51 @@ library(ggdist)
 
 # 1 Load data ====
 
-# Load age data
-ages <- readRDS('output/PB_clock_ages.rds') %>%
-  mutate(Born = floor(yr - Age)) %>%
-  mutate(across(c(AgeAccel, Born), 
+# Load epigenetic age data
+epi_dat <- readRDS('output/WH_combined_ages.rds') 
+
+# 2 Organize data by sex and tissue type ====
+
+# Summarize by individual and tissue type
+tissue_dat <- epi_dat %>%
+  group_by(BearID, Spec) %>%
+  summarize(AgeAccel = mean(AgeAccel), Born = mean(Born)) %>%
+  ungroup() %>%
+  # Scale and centre variables
+  mutate(across(c(AgeAccel, Born),
                 list(sc = function(x) as.vector(scale(x, center = T)))))
 
-# Filter blood samples only
-ages_blood <- ages %>%
-  filter(Spec == 'Blood')
-
-# Filter blood samples only
-ages_skin <- ages %>%
-  filter(Spec == 'Skin')
-
-# 2 Count number of samples ===
-
-# How many bears have multiple samples for blood?
-ages_blood %>% 
+# Summarize by individual and keep sex as variable
+sex_dat <- epi_dat %>%
   group_by(BearID) %>%
-  summarize(nSamples = n()) %>%
-  group_by(nSamples) %>%
-  summarize(count = n()) 
+  summarize(Sex = unique(Sex), AgeAccel = mean(AgeAccel), Born = mean(Born)) %>%
+  # Scale and centre variables
+  mutate(across(c(AgeAccel, Born),
+                list(sc = function(x) as.vector(scale(x, center = T)))))
 
-# # A tibble: 2 × 2
-# nSamples count
-# <int> <int>
-#   1     31
-#   2     4
-
-# How many bears have multiple samples for skin?
-ages_skin %>% 
-  group_by(BearID) %>%
-  summarize(nSamples = n()) %>%
-  group_by(nSamples) %>%
-  summarize(count = n()) 
-
-# A tibble: 5 × 2
-# nSamples count
-# <int> <int>
-#    1    44
-#    2     5
-#    4     1
-#    9     3
-#   10     1
-
-# 3 Fit models with single tissues ====
+# 3 Fit models with tissue and sex ====
 
 # Model age acceleration ~ year for skin only
-skin_mod <- brm(AgeAccel_sc ~ Born_sc + Sex + (Born_sc + Sex | BearID),
-                 family = gaussian, data = ages_skin,
-                 iter = 10000, warmup = 5000, chains = 4, cores = 4, 
-                 prior = prior(normal(0,1), class = b),
-                 control = list(adapt_delta = 0.99, max_treedepth = 20),
-                 backend = 'cmdstanr')
+tissue_m <- brm(AgeAccel_sc ~ Born_sc*Spec,
+                family = gaussian, data = tissue_dat,
+                iter = 10000, warmup = 5000, chains = 4, cores = 4, 
+                prior = prior(normal(0,1), class = b),
+                control = list(adapt_delta = 0.99, max_treedepth = 20),
+                backend = 'cmdstanr')
 
 # Model age acceleration ~ year for blood only
 # Weak effect... but sample size is really small
-blood_mod <- brm(AgeAccel_sc ~ Born_sc + Sex,
-                 family = gaussian, data = ages_blood,
+sex_m <- brm(AgeAccel_sc ~ Born_sc*Sex,
+                 family = gaussian, data = sex_dat,
                  iter = 10000, warmup = 5000, chains = 4, cores = 4, 
                  prior = prior(normal(0,1), class = b),
                  control = list(adapt_delta = 0.99, max_treedepth = 20),
                  backend = 'cmdstanr')
-
-# Probability of directional effect
-bayestestR::p_direction(blood_mod)
-bayestestR::p_direction(skin_mod)
-
-# Blood....
-# Parameter   |     pd
-# --------------------
-#    (Intercept) | 58.16%
-#   Born_sc     | 59.79%
-#  SexM        | 53.35%
-# 
-# Skin....
-# Parameter   |     pd
-# --------------------
-#    (Intercept) | 81.72%
-#   Born_sc     | 90.83%
-#  SexM        | 89.36%
 
 # 4 Check agreement between blood and skin samples ====
 
 # Which individuals have a combination of blood and skin in the same year?
-bears_by_year <- ages %>% 
+bears_by_year <- epi_dat %>% 
   group_by(BearID, Age) %>%
   summarize(nSamples = n()) %>%
   filter(nSamples > 1) %>%
@@ -106,10 +65,10 @@ bears_by_year <- ages %>%
 
 # Plot relationship between blood and skin age estimates when same bear was
 # aged at same time using both tissues
-ages  %>%
+epi_dat  %>%
   filter(BearID %in% bears_by_year) %>%
   arrange(BearID) %>%
-  filter(! BearID %in% c('X12697', 'X16239', 'X19212')) %>%
+  filter(! BearID %in% c('X12697', 'X16239', 'X19212', 'X19627')) %>%
   select(BearID, Spec, AgePredict) %>%
   pivot_wider(names_from = Spec, values_from = AgePredict) %>%
   ggplot(aes(x = Skin, y = Blood)) +
@@ -128,36 +87,26 @@ ages  %>%
 ggsave('blood_skin_agreement.tiff', plot = last_plot(), path = 'figures/supplementary/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
 
-# 5 Fit and plot age ~ birth year model with effect for tissue ====
-
-# Fit model
-tissue_mod <- brm(AgeAccel_sc ~ Born_sc + Spec + (Born_sc + Spec | BearID),
-                  family = gaussian, data = ages,
-                  iter = 10000, warmup = 5000, chains = 4, cores = 4, 
-                  prior = prior(normal(0,1), class = b),
-                  control = list(adapt_delta = 0.99, max_treedepth = 20),
-                  backend = 'cmdstanr')
+# 5 Plot age ~ birth year model with effects for tissue ====
 
 # Make new data for predicting
-# Skin
-nd <- expand_grid(BearID = NA,
-                  Spec = c('Blood', 'Skin'),
-                  Born_sc = seq(from = min(ages$Born_sc), 
-                                to = max(ages$Born_sc),
+nd_tissue <- expand_grid(Spec = c('Blood', 'Skin'),
+                  Born_sc = seq(from = min(tissue_dat$Born_sc), 
+                                to = max(tissue_dat$Born_sc),
                                 by = 0.05))
 
 # Extract fitted values
-f_tissue <- fitted(tissue_mod,
-                   newdata = nd,
+f_tissue <- fitted(tissue_m,
+                   newdata = nd_tissue,
                    probs = c(0.025, 0.975),
                    summary = F) %>%
   data.frame() %>%
   # Pivot
   pivot_longer(everything()) %>%
-  bind_cols(expand_grid(draws = 1:20000, nd)) %>%
+  bind_cols(expand_grid(draws = 1:20000, nd_tissue)) %>%
   # Rename and unscale
-  mutate(Born = Born_sc * sd(ages$Born) + mean(ages$Born),
-         AgeAccel = value * sd(ages$AgeAccel) + mean(ages$AgeAccel)) %>%
+  mutate(Born = Born_sc * sd(tissue_dat$Born) + mean(tissue_dat$Born),
+         AgeAccel = value * sd(tissue_dat$AgeAccel) + mean(tissue_dat$AgeAccel)) %>%
   select(Spec, Born, AgeAccel) %>%
   # Split by tissue
   group_by(Spec) %>%
@@ -176,29 +125,83 @@ line_sk <- f_tissue[[2]] %>%
 ggplot(f_tissue[[1]], aes(x = Born, y = AgeAccel)) +
   stat_lineribbon(data = f_tissue[[1]],
                   .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = 'red') +
+                  alpha = .1, size = 0, fill = '#e57368') +
   stat_lineribbon(data = f_tissue[[2]],
                   .width = seq(from = .03, to = .975, by = .03),
-                  alpha = .1, size = 0, fill = 'grey') +
-  geom_line(data = line_bl, colour = 'red') +
-  geom_line(data = line_sk, colour = '#808080') +
-  scale_colour_manual(values = c('red', '#808080'), name = 'Tissue type') +
+                  alpha = .1, size = 0, fill = '#8092a6') +
+  geom_line(data = line_bl, colour = '#d62d20') +
+  geom_line(data = line_sk, colour = '#536878') +
+  scale_colour_manual(values = c('#d62d20', '#536878'), name = 'Tissue type') +
   geom_jitter(data = ages, aes(x = Born, y = AgeAccel, colour = Spec), size = 3) +
   theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1.25),
         axis.text = element_text(size = 18, colour = 'black'),
         axis.title.y = element_text(size = 18, colour = 'black', vjust = 3),
         axis.title.x = element_text(size = 18, colour = 'black', vjust = -3),
-        legend.position = c(0.8, 0.15),
-        legend.text = element_text(size = 18, colour = 'black'),
-        legend.key = element_rect(colour = NA, fill = NA),
-        legend.title = element_text(size = 18, colour = 'black'),
-        legend.background = element_rect(fill = NA, colour = NA),
-        legend.box.background = element_rect(fill = NA, colour = NA),
+        legend.position = 'none',
         plot.margin = unit(c(0.25, 0.25, 0.75, 0.75), 'cm'),
         panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
-  ylab('Age acceleration (years)') + xlab('Year of birth')
+  ylab('Epigenetic age acceleration (years)') + xlab('Year of birth')
 
 # Save plot
 ggsave('accel_born_tissue_plot.tiff', plot = last_plot(), path = 'figures/supplementary/', 
+       device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
+
+# 6 Plot age ~ birth year model with effects for sex ====
+
+# Make new data for predicting
+nd_sex <- expand_grid(Sex = c('F', 'M'),
+                      Born_sc = seq(from = min(tissue_dat$Born_sc), 
+                                    to = max(tissue_dat$Born_sc),
+                                    by = 0.05))
+
+# Extract fitted values
+f_sex <- fitted(sex_m,
+                   newdata = nd_sex,
+                   probs = c(0.025, 0.975),
+                   summary = F) %>%
+  data.frame() %>%
+  # Pivot
+  pivot_longer(everything()) %>%
+  bind_cols(expand_grid(draws = 1:20000, nd_sex)) %>%
+  # Rename and unscale
+  mutate(Born = Born_sc * sd(sex_dat$Born) + mean(sex_dat$Born),
+         AgeAccel = value * sd(sex_dat$AgeAccel) + mean(sex_dat$AgeAccel)) %>%
+  select(Sex, Born, AgeAccel) %>%
+  # Split by tissue
+  group_by(Sex) %>%
+  group_split()
+
+# Get means of posterior for lines
+line_F <- f_sex[[1]] %>%
+  group_by(Born) %>%
+  summarize(AgeAccel = mean(AgeAccel))
+
+line_M <- f_sex[[2]] %>%
+  group_by(Born) %>%
+  summarize(AgeAccel = mean(AgeAccel))
+
+# Plot
+ggplot(f_sex[[1]], aes(x = Born, y = AgeAccel)) +
+  stat_lineribbon(data = f_sex[[1]],
+                  .width = seq(from = .03, to = .975, by = .03),
+                  alpha = .1, size = 0, fill = '#f78ab5') +
+  stat_lineribbon(data = f_sex[[2]],
+                  .width = seq(from = .03, to = .975, by = .03),
+                  alpha = .1, size = 0, fill = '#85b5f7') +
+  geom_line(data = line_F, colour = '#f15097') +
+  geom_line(data = line_M, colour = '#5097f1') +
+  scale_colour_manual(values = c('#f15097', '#5097f1'), name = 'Tissue type') +
+  geom_jitter(data = ages, aes(x = Born, y = AgeAccel, colour = Spec), size = 3) +
+  theme(panel.background = element_rect(colour = 'black', fill = 'white', linewidth = 1.25),
+        axis.text = element_text(size = 18, colour = 'black'),
+        axis.title.y = element_text(size = 18, colour = 'black', vjust = 3),
+        axis.title.x = element_text(size = 18, colour = 'black', vjust = -3),
+        legend.position = 'none',
+        plot.margin = unit(c(0.25, 0.25, 0.75, 0.75), 'cm'),
+        panel.grid = element_line(linewidth = 0.5, colour = '#e5e5e5')) +
+  ylab('Epigenetic age acceleration (years)') + xlab('Year of birth')
+
+# Save plot
+ggsave('accel_born_sex_plot.tiff', plot = last_plot(), path = 'figures/supplementary/', 
        device = 'tiff', dpi = 300, height = 12, width = 14, units = 'cm', bg = 'white')
 
