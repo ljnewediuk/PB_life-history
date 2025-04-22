fe43b516f90a456505ff1276e604daa905976902
ljnewediuk <j.newedi@gmail.com>
2025-04-22 17:43:42 -0500

    clean upp

--- R/07-glm_Models.R
+++ R/07-glm_Models.R
@@ -56,35 +56,9 @@ accel_born_mod <-  lm(AgeAccel_sc ~ Born_sc, data = epi_dat)
 accel_fr_mod <- lm(AgeAccel_sc ~ FirstRepro_sc, data = lh_epi_dat)
 
 # 3 Model lifetime reproductive success ~ age at first reproduction ====
-
-# Negative binomial
 lrs_fr_mod <- MASS::glm.nb(LRS ~ FirstRepro*Born, data = lh_pop_dat)
-# Poisson
-lrs_fr_mod <- glm(LRS ~ FirstRepro*Born, data = lh_pop_dat, family = poisson(link = 'log'))
-
-# Make new data for predicted distribution
-pred_dat <- data.frame(FirstRepro = rep(seq(from = min(lh_pop_dat$FirstRepro), 
-                                            to = max(lh_pop_dat$FirstRepro), 
-                                            length.out = 100), 3),
-                      Born = c(rep(1965, times = 50), 
-                               rep(1990, times = 50), 
-                               rep(2000, times = 50)))
-# Predict
-preds <- predict(lrs_fr_mod, new_dat, type = 'response', se.fit = T)
-
-# Add predictions and confidence intervals
-pred_dat$LRS_pred <- preds$fit
-pred_dat$LRS_lower <- preds$fit - preds$se.fit*2
-pred_dat$LRS_upper <- preds$fit + preds$se.fit*2
-
-# Plot
-pred_dat %>%
-  ggplot(aes(x = FirstRepro, y = LRS_pred, group = Born)) +
-  geom_jitter(data = lh_pop_dat, aes(x = FirstRepro, y = LRS, colour = Born)) +
-  geom_ribbon(aes(x = FirstRepro, ymin = LRS_lower, ymax = LRS_upper), alpha = 0.3) +
-  geom_line()
 
 # Save models
-saveRDS(accel_born_mod, 'models/accel_born_mod.rds')
-saveRDS(accel_fr_mod, 'models/accel_fr_mod.rds')
-saveRDS(lrs_fr_mod, 'models/lrs_fr_mod.rds')
+saveRDS(accel_born_mod, 'models/accel_born_mod_lm.rds')
+saveRDS(accel_fr_mod, 'models/accel_fr_mod_lm.rds')
+saveRDS(lrs_fr_mod, 'models/lrs_fr_mod_nb_glm.rds')
